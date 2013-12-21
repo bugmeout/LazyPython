b283174b6899a6c9d17247ef01784c0345ab2377
bugmeout <hicode@foxmail.com>
2013-12-21 22:08:18 +0800

    render_build

--- ex50/gothonweb/bin/app.py
+++ ex50/gothonweb/bin/app.py
@@ -1,16 +1,30 @@
 import web
-urls = ('/', 'index')
+urls = ('/', 'index','/test','test','/good','good')
+
  
 app = web.application(urls,globals())
 
-render = web.template.render('templates/')
-
+#render = web.template.render('templates/')
+render2 = web.template.render('templates/')
 
 class index:
 	def GET(self):
 		greeting ="hello world!"
+		fool ="I am fool"
 		#return greeting
-		return render.index(greeting = greeting)
+		return render2.foo(fool = fool)
+		#return render.index(greeting = greeting)
+
+class test:
+	def GET(self):
+		sayhello = "i am hello"
+		return render2.foo(fool = sayhello)
+
+class good:
+	def GET(self):
+		good = "good enough"
+		return render2.index(greeting = good)
+
 		
 if __name__ == "__main__":
 	app.run()
