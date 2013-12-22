2ae62e5b3e1dc74472ef0f2994ad787526a87652
bugmeout <hicode@foxmail.com>
2013-12-22 15:29:15 +0800

    Update NoneNulltest.py
    Both 'None' and '' are false, but they're NOT same.

--- ex51/gothonweb/bin/NoneNulltest.py
+++ ex51/gothonweb/bin/NoneNulltest.py
@@ -4,15 +4,15 @@ b = None
 if(a):
 	print "A is True logically",
 else:
-	print "A is False"
+	print "A is False"   #print
 
 if(b):
 	print "B is True"
 else:
-	print "B is False"
+	print "B is False"  #print
 
 
 if(a == b):
 	print "a ==b",
 else:
-	print "a != b"
+	print "a != b"  #print
