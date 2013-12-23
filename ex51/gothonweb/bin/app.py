81d2bbf96a9aa8dc0f4c89923e2d0da03a473ecb
bugmeout <hicode@foxmail.com>
2013-12-23 16:29:19 +0800

    添加框架

--- ex51/gothonweb/bin/app.py
+++ ex51/gothonweb/bin/app.py
@@ -8,10 +8,10 @@
 #if(a) 与if(b)都是False
 import web
 #urls = ('hello/','Index')
-urls = ('/hello','Index','/test','test')
+urls = ('/hello','Index','/test','test','/postway','postway')
 app = web.application(urls,globals())
 
-render = web.template.render('templates/')
+render = web.template.render('templates/',base='layout')
 
 class Index():
 	def GET(self):
@@ -46,6 +46,24 @@ class test(object):
 		#哈哈，将来可以使用这个方法来调试了。
 		#因为这里的return就return给了浏览器
 			return "Error: greet is required."
+
+
+class postway(object):
+	def GET(self):
+		return render.hello_form()
+	def POST(self):
+
+
+		#How to receive the posted data.........
+
+
+
+
+
+		#this sentence is meaningless.
+		record = web.input(name='Nobody', greet = 'Hello')
+		greeting = "%s,%s" % (record.name,record.greet)
+		return render.index(greeting = greeting)		
 
 
 
